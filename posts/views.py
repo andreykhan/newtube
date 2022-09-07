db82a566741a0497f5806dde96bbb6463cf5efaf
Andrey Khan <Andrey.Khan@kcell.kz>
2022-09-07 11:30:40 +0600

    added templates

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('main page')
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, name):
-    return HttpResponse(f'{name} page')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
